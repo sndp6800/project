6d892285a4c0361a884dfd4eea14f30b12111a38
sndp6800 <sandeeppandey6800@gmail.com>
2021-11-30 02:31:15 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FormControl,FormGroup,Validators,FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ca-webpage';
+  reg="";
+  pass="";
+  showReg()
+  { console.log("",this.reg)}
+  showPass()
+  {console.log("",this.pass); }
+  contentHighlighted:boolean=false;
+  loginForm= new FormGroup({
+    registration: new FormControl('',Validators.required),
+    password: new FormControl('',Validators.required),
+    //password: new FormControl(null,[Validators.required, Validators.minLength(8)])
+  })
+
+  get registration(){
+    return this.loginForm.get('registration')
+  }
+  get password(){
+    return this.loginForm.get('password')
+  } 
 }
